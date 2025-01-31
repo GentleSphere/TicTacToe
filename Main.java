7d1cb9b81cd034a6366b6ee73d07bd071961cb1d
GentleSphere <joshuasantiago8888@gmail.com>
2025-01-31 11:19:28 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+enum Status{
+    WIN,
+    DRAW,
+    CONTINUE
+}
+
+public class Main {
+    public static void main(String[] args){
+        return;
+    }
+
+    //Receives and displays the move by the player
+    private void play(){
+
+    }
+    //Displays the current status of the game
+    private void printStatus(){
+
+    }
+    //Prints the current board
+    private void printBoard(){
+
+    }
+    //Checks if the move made was valid
+    private bool validMove(){
+
+    }
+
+}
