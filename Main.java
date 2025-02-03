18b800b3d83bf8e10cc3183419165e990809c5ba
GentleSphere <joshuasantiago8888@gmail.com>
2025-02-03 12:42:15 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 enum Status{
     WIN,
     DRAW,
@@ -5,12 +6,25 @@ enum Status{
 }
 
 public class Main {
+
+    int turn = 1;//Determines whose turn it is
+    int box;//Determines which box each method affects
+    int[] board = {0,0,0,0,0,0,0,0,0}; //Determines which player took which square
     public static void main(String[] args){
+        //printBoard();
         return;
     }
 
     //Receives and displays the move by the player
     private void play(){
+        if(turn == 1){
+            System.out.println("Player 1: Please make a move:");
+
+        }
+        else{
+            System.out.println("Player 2: Please make a move");
+        }
+
 
     }
     //Displays the current status of the game
@@ -19,11 +33,54 @@ public class Main {
     }
     //Prints the current board
     private void printBoard(){
-
+        //Use if statement later to show what is in the middle of each square
+        System.out.println(" _______________________ ");
+        System.out.println("|       |       |       |");
+        for(int i = 0; i <= 2; i++){
+            if(board[i] == 0){
+                System.out.println("|       |");
+            }
+            else if(board[i] == 1){
+                System.out.println("|   X   |");
+            }
+            else{
+                System.out.println("|   O   |");
+            }
+        }
+        System.out.println("|       |       |       |");
+        System.out.println("|_______|_______|_______|");
+        System.out.println("|       |       |       |");
+        for(int i = 3; i <= 5; i++){
+            if(board[i] == 0){
+                System.out.println("|       |");
+            }
+            else if(board[i] == 1){
+                System.out.println("|   X   |");
+            }
+            else{
+                System.out.println("|   O   |");
+            }
+        }
+        System.out.println("|_______|_______|_______|");
+        System.out.println("|       |       |       |");
+        for(int i = 6; i <= 9; i++){
+            if(board[i] == 0){
+                System.out.println("|       |");
+            }
+            else if(board[i] == 1){
+                System.out.println("|   X   |");
+            }
+            else{
+                System.out.println("|   O   |");
+            }
+        }
+        System.out.println("|_______|_______|_______|");
     }
     //Checks if the move made was valid
+    /* 
     private bool validMove(){
 
     }
+    */
 
 }
